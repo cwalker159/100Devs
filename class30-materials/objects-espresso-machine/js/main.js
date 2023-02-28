0bc4ec287b15ae689529674c89146c92d188fa24
Carl R. Walker Jr <96443329+cwalker159@users.noreply.github.com>
2023-02-28 01:09:29 -0500

    Add files via upload

---
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -0,0 +1,22 @@
+//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class MakeEspresso{
+    constructor(color,make,model,price){
+      this.color = color 
+      this.make = make 
+      this.model = model
+      this.price = price
+    }
+    
+    turnOn(){
+      alert('Hoy, I am On')
+    }
+    steam(){
+      alert('ITSS SUMMBER BABY')
+    }
+    steam(){
+        alert('GOOD STUFF BABY')
+    }
+  }
+  
+  let gaggia = new MakeEspresso('red','Gaggia','Classic Pro', 400)
+  
