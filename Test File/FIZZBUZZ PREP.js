0d6a20006a73f6171b409f7085bcd9c49e163acb
Carl R. Walker Jr <96443329+cwalker159@users.noreply.github.com>
2023-02-28 01:27:45 -0500

    Add files via upload

---
+++ Test File/FIZZBUZZ PREP.js
@@ -0,0 +1,14 @@
+//PREP 
+//A number 1 to number, if num div by 3 = fizz, if num div by 5 = buzz, div by 3&5 = fizzBuzz
+//P: always num, whole num, positive num, 
+//R: console
+//E: 
+function fizzBuzz(num){
+    //loop
+    //conditionals %3&5, %3, %5, 
+    //console.log num or fizz, buzz, fizzBuzz
+}
+
+fizzBuzz(5) //1,2,fizz,4,buzz
+fizzBuzz(3) //1,2,fizz
+fizzBuzz(15)//1,2,fizz,4,buzz, fizz,7,8,fizz,buzz,11,fiz,13,14,fizzBuzz
